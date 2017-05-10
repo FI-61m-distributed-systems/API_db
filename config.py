1d3650ef37f89c28e8f62dc499f2dc42cf4a42c2
polyvika <medvika@ukr.net>
2017-05-10 15:36:02 +0300

    add setting for db and script for getting config

---
+++ config.py
@@ -0,0 +1,39 @@
+import psycopg2
+from get_parametra import config
+ 
+def connect():
+    
+    conn = None
+    try:
+      
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(
+        """
+        
+        CREATE TABLE account
+        (
+          id serial NOT NULL,
+          "login" character varying(50) NOT NULL,
+          "password" character varying(50) NOT NULL,
+          email character varying(50) NOT NULL,
+          money integer DEFAULT 0,
+          CONSTRAINT account_pkey PRIMARY KEY (id),
+          CONSTRAINT account_login_key UNIQUE (login)
+        )
+        WITH (
+          OIDS=FALSE
+        );
+        ALTER TABLE account OWNER TO postgres;
+        """)
+        conn.commit() 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)        
+    finally:
+        if conn is not None:
+            conn.close()
+             
+if __name__ == '__main__':
+    connect()
