4320012c7c492523afb2d4fcdfff64308b13ffe7
polyvika <medvika@ukr.net>
2017-05-21 16:30:00 +0300

    change return

--- stub.py
+++ stub.py
@@ -1,25 +1,25 @@
 import hashlib
 
-def get_login():
+def get_login(data):
     login= raw_input("login ")
     return login
 
-def money():
+def money(data):
     money= raw_input("money ")
     return money
 
 def send_ok(data):
-    print "Welcom ", data
+    return 200
 
 def send_err(data):
-    print "ERROR ", data
+    return 500
 
-def get_password():
+def get_password(data):
     password = raw_input("password ")
     h = hashlib.md5(password)
     p = h.hexdigest()
     return p
 
-def get_email():
+def get_email(data):
     email= raw_input("email ")
     return email 
