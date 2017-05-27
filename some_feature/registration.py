aba084f55c020da8876a4e1ad7e2eda25d23ed92
polyvika <medvika@ukr.net>
2017-05-27 22:46:34 +0300

    new dir

---
+++ some_feature/registration.py
@@ -0,0 +1,17 @@
+import psycopg2
+from connection import connection
+from stub import send_err
+
+def reg(login, password, email):
+    try:
+        connect = connection()    
+        cursor = connect.cursor()
+    except:
+        send_err(1)
+    try:    
+        cursor.execute(
+        """INSERT INTO account  ( id,login, password, email, money,game_config) VALUES(default, %s , %s, %s,default,default);""",
+        (login, password, email))
+        connect.commit()		
+    except (Exception, psycopg2.Error) as err:
+        send_err(err) 
